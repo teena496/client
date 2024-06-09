f0dcfbd723c27519977a194a555fc24fc049d212
Teena Elsa Thomas <teena496@outlook.com>
2024-06-09 12:10:29 -0400

    Initial client commit

---
+++ src/scenes/global/NavBar.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Badge, Box, IconButton } from "@mui/material";
+import {
+  PersonOutline,
+  ShoppingBagOutlined,
+  SearchOutlined,
+  MenuOutlined,
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { shades } from "../../theme";
+import { setIsCartOpen } from "../../state";
+
+export const NavBar = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
+
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      width="100%"
+      height="60px"
+      backgroundColor="rgba(255,255,255,0.95)"
+      color="black"
+      top="0"
+      left="0"
+      zIndex="1"
+    >
+      <Box
+        width="80%"
+        margin="auto"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Box
+          onClick={() => navigate("/")}
+          sx={{ "&:hover": { cursor: "pointer" } }}
+          color={shades.secondary[500]}
+        >
+          ECOMMERCE
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          columnGap="20px"
+          zIndex="2"
+        >
+          <IconButton sx={{ color: "black" }}>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton sx={{ color: "black" }}>
+            <PersonOutline />
+          </IconButton>
+          <Badge
+            badgeContent={cart.length}
+            color="secondary"
+            sx={{
+              "& .MuiBadge-badge": {
+                right: 5,
+                top: 5,
+                padding: "0 4px",
+                height: "14px",
+                minWidth: "13px",
+              },
+            }}
+          >
+            <IconButton
+              sx={{ color: "black" }}
+              onClick={() => dispatch(setIsCartOpen({}))}
+            >
+              <ShoppingBagOutlined />
+            </IconButton>
+          </Badge>
+          <IconButton sx={{ color: "black" }}>
+            <MenuOutlined />
+          </IconButton>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
---
+++ src/scenes/itemDetails/ItemDetails.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ItemDetails = () => {
+  return (
+    <div>ItemDetails</div>
+  )
+}
